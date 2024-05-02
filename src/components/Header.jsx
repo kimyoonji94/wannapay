52dc2615e6ecc618ce6698ee762bf60dd8e72cef
Jiwoon <kfromh0136@Naver.com>
2024-05-02 10:04:03 +0900

    [style] code style refactoring

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,17 @@
 //import member from './page/member';
 
 export const Header = () => {
-    return (
-    <header className="header_box">
-      <div className="container">
-        <div className="lnb_box">
+  return (
+    <header className='header_box'>
+      <div className='container'>
+        <div className='lnb_box'>
           {/* <Link to="/page/member"></Link> */}
-          <div className="lnb_box_top">
+          <div className='lnb_box_top'>
             <div>회원관리</div>
             <div>최고관리자님</div>
             <div>로그아웃</div>
           </div>
-          <div className="lnb_box_botm">
+          <div className='lnb_box_botm'>
             <div>수기결제</div>
             <div>결제내역</div>
             <div>URL결제</div>
@@ -20,5 +20,5 @@ export const Header = () => {
         </div>
       </div>
     </header>
-    )
-}
+  );
+};
